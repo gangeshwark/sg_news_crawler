900c0a25321bd1b6cefbb750299bf882ba7a1644
gangeshwark <gangeshwark@gmail.com>
2020-02-11 01:02:22 +0800

    Added database

---
+++ db_setup.py
@@ -0,0 +1,25 @@
+from peewee import *
+from peewee import SqliteDatabase
+import datetime
+
+db = SqliteDatabase('new_articles.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class NewsArticle(BaseModel):
+    id = TextField(unique=True)
+    headline = TextField()
+    URL = TextField()
+    created_date = DateTimeField(default=datetime.datetime.now)
+    source_name = TextField()
+
+
+if __name__ == '__main__':
+    db.connect()
+    # db.drop_tables([NewsArticle])
+    db.create_tables([NewsArticle])
+    db.close()
