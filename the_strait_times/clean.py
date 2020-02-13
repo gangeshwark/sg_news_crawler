53a849f41af6de92b8a316e98aa2a6659fe6c496
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2020-02-13 12:38:05 +0800

    Update strait times

---
+++ the_strait_times/clean.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = pd.read_csv('data/all_data_new.csv', index_col='index')
+print(data)
+data.drop_duplicates(['source_url'], inplace=True)
+data.reset_index(inplace=True)
+data.drop(['index'], inplace=True, axis=1)
+print(data)
+
+data.to_csv('data/all_data_new.csv', index_label='index')
+data.to_excel('data/all_data_new.xlsx', index_label='index')
