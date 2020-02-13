e4a32e8c75e8c47d04c005631674be21ad4029b6
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2020-02-13 12:40:28 +0800

    Update strait times

--- the_strait_times/clean.py
+++ the_strait_times/clean.py
@@ -6,6 +6,7 @@ data.drop_duplicates(['source_url'], inplace=True)
 data.reset_index(inplace=True)
 data.drop(['index'], inplace=True, axis=1)
 print(data)
+data['source_url'] = 'https://www.straitstimes.com' + data['source_url'].astype(str)
 
 data.to_csv('data/all_data_new.csv', index_label='index')
 data.to_excel('data/all_data_new.xlsx', index_label='index')
